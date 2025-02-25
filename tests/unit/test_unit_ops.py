742ddcd34e9d0afa1577066ec96d73d2b871b397
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2025-02-25 14:15:37 +0100

    Pass first basic test

--- tests/unit/test_unit_ops.py
+++ tests/unit/test_unit_ops.py
@@ -1,3 +1,11 @@
+import pint
+import pint.testing
+
+from ici_phd_rse.unit_aware import add_unit_aware
+
+Q = pint.get_application_registry().Quantity
+
+
 def test_add_unit_aware():
     a = Q(1.0, "m")
     b = Q(1.0, "cm")
